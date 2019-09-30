5f8ca8a68be7338b680698817b3b9a92228e51b3
Romulo Rocha <romulorocha063@gmail.com>
2019-09-30 18:33:28 -0300

    testing email duplicated

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,6 +2,14 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const { email } = req.body;
+
+    const checkDuplicateEmail = await User.findOne({ where: { email } });
+
+    if (checkDuplicateEmail) {
+      res.status(400).json({ error: 'Duplicated email' });
+    }
+
     const user = await User.create(req.body);
 
     return res.json(user);
